cccf29a957d827aa5332caf90118e590d11951b3
Landry Simo <landrysimo99@gmail.com>
2024-12-10 11:36:13 +0100

    Adds crouching and aiming up states
    Extends player movement with crouching and aiming up functionalities.
    Improves player state management to handle new states.
    Updates animations to include crouching and aiming up poses.

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -9,7 +9,11 @@ enum PlayerState {
 	RUN_FIRE,
 	JUMP,
 	RUN_FIRE_DOWN,
-	RUN_FIRE_UP
+	RUN_FIRE_UP,
+	UP,
+	UP_FIRE,
+	CROUCH,
+	CROUCH_FIRE
 }
 
 @onready var anim = $AnimatedSprite2D
@@ -28,7 +32,10 @@ const JUMP_VELOCITY = -300.0
 
 var _state: PlayerState
 var facing_direction = 1
+var direction : Vector2
 var in_fire: bool = false
+var in_up: bool = false
+var in_crouch: bool = false
 
 
 func _ready() -> void:
@@ -52,30 +59,28 @@ func _physics_process(delta: float) -> void:
 
 func _manageInputs():
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") or jumpButton.is_pressed() and is_on_floor():
+	if (Input.is_action_just_pressed("ui_accept") or jumpButton.is_pressed()) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	
+	in_fire = Input.is_action_pressed("fire") or fireButton.is_pressed()
+	in_up = Input.is_action_pressed("ui_up")
+	in_crouch = Input.is_action_pressed("ui_down")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	if direction.x:
+		velocity.x = direction.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if direction != 0:
-		facing_direction = direction
+	if direction.x != 0:
+		facing_direction = direction.x
 
 	anim.flip_h = facing_direction < 0
 
-	_toggleFire()
 
-
-func _toggleFire() -> void:
-	in_fire = false
-
-	if Input.is_action_pressed("fire") or fireButton.is_pressed():
-		in_fire = true
 	
 
 func _applyGravity(delta: float) -> void:
@@ -84,25 +89,36 @@ func _applyGravity(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 func _manageState() -> void:
-	if is_on_floor() and velocity.x == 0.0 and not in_fire:
-		_changeState(PlayerState.IDLE)
-
-	elif is_on_floor() and velocity.x != 0.0 and not in_fire:
-		_changeState(PlayerState.RUN)
+	if is_on_floor():
+		if in_up:
+			if velocity.x != 0.0:
+				_changeState(PlayerState.RUN_FIRE_UP)
+			else:
+				_changeState(PlayerState.UP)
+		elif in_crouch:
+			if velocity.x != 0.0:
+				_changeState(PlayerState.RUN_FIRE_DOWN)
+			else:
+				_changeState(PlayerState.CROUCH)
+		elif velocity.x == 0.0:
+			_changeState(PlayerState.IDLE)
+		elif velocity.x != 0.0:
+			_changeState(PlayerState.RUN)
+		elif in_fire:
+			if velocity.x != 0.0:
+				_changeState(PlayerState.RUN_FIRE)
+			else:
+				_changeState(PlayerState.FIRE)
 
 	elif not is_on_floor():
 		_changeState(PlayerState.JUMP)
 
-	elif is_on_floor() and velocity.x != 0.0 and in_fire:
-		_changeState(PlayerState.RUN_FIRE)
-
-	elif is_on_floor() and in_fire:
-		_changeState(PlayerState.FIRE)
+	
 
 	
 func _changeState(state: PlayerState) -> void:
 	if state != _state:
-		# print("change state ", PlayerState.keys()[_state], "->", PlayerState.keys()[state])
+		print("change state ", PlayerState.keys()[_state], "->", PlayerState.keys()[state])
 
 		_state = state
 		_updateAnim(state)
@@ -124,6 +140,14 @@ func _updateAnim(state: PlayerState) -> void:
 			anim.play("run_fire_down")
 		PlayerState.RUN_FIRE_UP:
 			anim.play("run_fire_up")
+		PlayerState.UP:
+			anim.play("up")
+		PlayerState.UP_FIRE:
+			anim.play("up_fire")
+		PlayerState.CROUCH:
+			anim.play("crouch")
+		PlayerState.CROUCH_FIRE:
+			anim.play("crouch_fire")
 
 func _on_bullet_timer_timeout() -> void:
 	if not in_fire:
